e5bebf0c9d0de42b9085a2173dafc2499bd1b5a5
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-14 12:24:53 +0545

    Add files via upload

---
+++ Print name.c
@@ -0,0 +1,15 @@
+/*
+Author/Student name:Sunita
+Subject:Programming fundamental
+Roll no:
+Lab Sheet:1
+Program:Write a C program print your name
+date:
+*/
+#include<stdio.h>//printf(),scanf()
+#include<conio.h>clrscr(),getch()
+int main(){
+printf("sunita");
+getch();//hold screen o/p
+return 0;//return(0);
+}
