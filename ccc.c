2672c8cc6aa073c3d5eae0d0bb28fe12d52545be
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-14 12:58:13 +0545

    Add files via upload

---
+++ ccc.c
@@ -0,0 +1,18 @@
+/*
+Student name:Sunita
+Subject:Programming Fundamental
+Roll no :
+lab sheet no:1
+Date:14/11/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char name[10];
+printf("what is your name ?");
+scanf("%s",&name);
+printf("Name of user is %s",name);
+getch();
+return(0);
+}
