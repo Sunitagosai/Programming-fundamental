e348965780703c7012383aa558c6a2949759226a
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-14 12:25:23 +0545

    Update Print name.c

--- Print name.c
+++ Print name.c
@@ -7,7 +7,7 @@ Program:Write a C program print your name
 date:
 */
 #include<stdio.h>//printf(),scanf()
-#include<conio.h>clrscr(),getch()
+#include<conio.h>//clrscr(),getch()
 int main(){
 printf("sunita");
 getch();//hold screen o/p
