e0417787eb129296549c5bfe262adb2a342d0c6b
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-16 10:52:14 +0545

    Add files via upload

---
+++ product.c
@@ -0,0 +1,21 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab sheet no:02
+Program:WAP to print product of two numbers ask from user
+Date:16 Nov,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,pro;
+printf("enter first number:");
+scanf("%d",&a);
+printf("enter second number:");
+scanf("%d",&b);
+pro=a*b;
+printf("pro of two numbers is:%d",pro);
+getch();
+return(0);
+}
