ed17d820a566e1fce97008597348395a7d049674
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-16 12:45:15 +0545

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,21 @@
+/*
+Student name:Sunita Gosai
+Subject:Programming Fundamental
+Lab sheet no:02
+Program:WAP to print divide of two numbers ask from user
+Date:16 NOv,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,divide;
+printf("enter first number:");
+scanf("%f",&a);
+printf("enter second number:");
+scanf("%f",&b);
+divide=a/b;
+printf("divide of two numbers is:%f",divide);
+getch();
+return(0);
+}
