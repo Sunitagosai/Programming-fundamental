64ffb0e42c3da07906c71171afba4c8a822e8062
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-18 10:39:34 +0545

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,20 @@
+/*
+Student name:Sunita Gosai
+Subject:Programming Fundamental
+Lab sheet no:02
+Program:WAP to print sum of two numbers ask from users
+Date:18 NOv,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,sum;
+printf("enter first number:");
+scanf("%f",&a);
+printf("enter second number:");
+scanf("%f",&b);
+sum=a+b;
+printf("sum of two number is:%f",sum);
+getch();
+return(0);
+}
