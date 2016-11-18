16a2c4b345d684eb67e87d483dead7eac421f64d
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-18 12:34:59 +0545

    Add files via upload

--- area.c
+++ area.c
@@ -1,20 +1,20 @@
 /*
-Students Name:Sunita Gosai
-Subject:Programming Fundamental
-Lab sheet no:02
-Program:WAP to print area of triangle,base & height are asked from users
-Date:16 Nov,2016
+Student Name:Sunita Gosai
+Subject :Programming Fundamental
+Lab sheet no:03
+Program:WAP to calculate area and circumference of circle having radius
+Date:18 Nov,2016
 */
 #include<stdio.h>
 #include<conio.h>
 int main(){
-float b,h,area;
-printf("enter first number:");
-scanf("%f",&b);
-printf("enter second number:");
-scanf("%f",&h);
-area=0.5*b*h;
-printf("area of triangle is:%f",area);
+float r,area,circumference;
+printf("Enter the radius:\n");
+scanf("%f",&r);
+area=(3.14)*r*r;
+circumference=2*(3.14)*r;
+printf("Area of circle is:%f\n",area);
+printf("Circumference of circle is:%f",circumference);
 getch();
 return(0);
 }
