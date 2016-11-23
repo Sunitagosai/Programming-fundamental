47f64bc29cdf8fcc5a1b956c1bd04176aa18b65d
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-23 12:42:19 +0545

    Add files via upload

---
+++ cha.c
@@ -0,0 +1,18 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet No :05
+Program:WAP to find ASCII value of any character assigned from users
+Date:23 Nov,2016
+*/
+#include<stdio.h>
+int main(){
+char character;
+int asciivalue;
+printf("Enter a Character:");
+scanf("%c",&character);
+asciivalue=character;
+printf("Enter a asciivalue: %d",asciivalue);
+getch();
+return(0);
+}
