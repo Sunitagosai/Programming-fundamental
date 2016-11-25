3eb75a0661f1fa28dc552a854d235780a47df768
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-25 11:01:32 +0545

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,24 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet No:06
+Program:WAP to check the number is odd or even
+Date:25 Nov,2016
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    if(n%2==0)
+        {
+            printf("Number is even");
+    }
+    else
+        {
+            printf("Number is odd");
+    }
+
+    return 0;
+    }
+
