e20685571dd5d36bf2b60dbe475b2f3544bde149
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-25 13:09:17 +0545

    Add files via upload

---
+++ Logical.c
@@ -0,0 +1,21 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet No:06
+Program:WAP to read a number from user
+Date:25 Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a=5,b=4;
+if(a==5&&b==5)
+{
+    printf("true");
+}
+    else
+    {
+        printf("false");
+    }
+}
+
