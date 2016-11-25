136de1f21a4e4a7645d068f94f9c4437bb12fc3e
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-25 10:40:13 +0545

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,24 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet No:06
+Program:WAP to check the number is positive or negative
+Date:25 Nov,2016
+*/
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+if(n>0)
+{
+printf("Number is positive:");
+}
+else
+{
+printf("Number is negative:");
+}
+getch();
+return 0;
+}
+
