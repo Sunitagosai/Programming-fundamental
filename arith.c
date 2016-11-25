6e1b22fd2f236a1c81537dc1f4c8c9f814590317
Sunitagosai <Sunita.gosai143@gmail.com>
2016-11-25 10:12:58 +0545

    Add files via upload

---
+++ arith.c
@@ -0,0 +1,28 @@
+/*
+Student Name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet No:06
+Program:WAP to print arithmetic operation
+Date:25 Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,Add,Sub,Multi,Div,Modulus;
+printf("Enter the first number:\n");
+scanf("%d",&a);
+printf("Enter the second number:\n");
+scanf("%d",&b);
+Add=a+b;
+Sub=a-b;
+Multi=a*b;
+Div=a/b;
+Modulus=a%b;
+printf("The Add number is:%d\n,Add");
+printf("The Sub number is:%d\n,Sub");
+printf("The Multi number is:%d\n,Multi");
+printf("The Div number is:%d\n,Div");
+printf("The Modulus number is:%d\n,Modulus");
+getch();
+return(0);
+}
