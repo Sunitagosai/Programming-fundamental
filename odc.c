5dd09b4c456de5e300601da9b5a9acf1d615e2d8
Sunitagosai <Sunita.gosai143@gmail.com>
2016-12-07 10:38:46 +0545

    Add files via upload

---
+++ odc.c
@@ -0,0 +1,32 @@
+/*
+Student name:Sunita Gosai
+Subject:Programming Fundamental
+Lab Sheet no:11
+Program:WAP to check no. odd or even
+Date:7 Dec,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,c;
+printf("Enter the first number:\n");
+scanf("%d",&a);
+printf("Enter the second number:\n");
+scanf("%d",&b);
+printf("Enter the third number:\n");
+scanf("%d",&c);
+if(a>=b && a>=c)
+{
+    printf("Largest is:%d\n",a);
+}
+else if (b>=a && b>=c)
+{
+    printf("Largest is:%d",b);
+}
+else
+{
+    printf("Largest is:%d",c);
+}
+getch();
+return(0);
+}
