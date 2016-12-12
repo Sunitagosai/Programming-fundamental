1d3dc77cf34b6ecf17c7b4c49a2e9aae5f84559f
Sunitagosai <Sunita.gosai143@gmail.com>
2016-12-12 11:44:09 +0545

    Add files via upload

--- odc.c
+++ odc.c
@@ -1,32 +1,16 @@
-/*
-Student name:Sunita Gosai
-Subject:Programming Fundamental
+/*Student Name:Sunita Gosai
+Subject:programming Fundamental
 Lab Sheet no:11
-Program:WAP to check the largest number
-Date:7 Dec,2016
+Program:WAP to find the number is odd or even using ternary operator
+Date:12 Dec,2016
 */
 #include<stdio.h>
 #include<conio.h>
 int main(){
-int a,b,c;
+int n;
 printf("Enter the first number:\n");
-scanf("%d",&a);
-printf("Enter the second number:\n");
-scanf("%d",&b);
-printf("Enter the third number:\n");
-scanf("%d",&c);
-if(a>=b && a>=c)
-{
-    printf("Largest is:%d\n",a);
-}
-else if (b>=a && b>=c)
-{
-    printf("Largest is:%d",b);
-}
-else
-{
-    printf("Largest is:%d",c);
-}
+scanf("%d",&n);
+(n%2==0)?printf("%d is even",n):printf("%d is odd",n);
 getch();
 return(0);
 }
