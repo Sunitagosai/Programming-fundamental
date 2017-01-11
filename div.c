84d1f1383f82b1c8f2873aced24c9a8738964cd8
Sunitagosai <Sunita.gosai143@gmail.com>
2017-01-11 11:06:40 +0545

    Add files via upload

---
+++ div.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b;
+printf("Enter the number\n");
+scanf("%d",&a);
+b=a/10;
+printf("The result divide the number by 10 is %d\n",b);
+getch();
+return(0);
+}
