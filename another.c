f1f86a5656e6d08b82d23dd4d8f5fcf0d958eea6
Sunitagosai <Sunita.gosai143@gmail.com>
2017-01-11 11:06:05 +0545

    Add files via upload

---
+++ another.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,c;
+printf("Enter the first number\n");
+scanf("%d",&a);
+printf("Enter the second number\n");
+scanf("%d",&b);
+c=a/b;
+printf("The divide number is %d",c);
+getch();
+return(0);
+}
