f594f877b1cdae30ea8634ba5a36d8f4e5c45460
Sunitagosai <Sunita.gosai143@gmail.com>
2017-02-01 11:07:20 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+/*
+Student Name:Sunita Gosai
+Subject:programming Fundamental
+Program:WA
+Lab Sheet No:24
+Date:1 Feb,2017
+*/
+#include<stdio.h>
+int main(){
+	int a[5]={1,2,3,4,5},i;
+	for(i=0;i<5;i++)
+		{
+			printf("a[%d]=%d\n",i,a[i]);
+		}
+		return(0);
+	}
